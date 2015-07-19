b8ed12967d6545d388de47ea1b493533983dc9c7
Paul Reioux <reioux@gmail.com>
2015-07-19 10:28:36 +0800

    Flounder: Add IntelliActive CPU governor
    Change-Id: I660e5d3830faf5f84761c3cbff2c7eb585263fb6
    
    Conflicts:
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -413,6 +413,9 @@ extern struct cpufreq_governor cpufreq_gov_yankactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKDEMAND)
 extern struct cpufreq_governor cpufreq_gov_yankdemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankdemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
 #endif
 
 
