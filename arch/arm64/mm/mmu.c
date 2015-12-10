a3af033335b6bda712d2f5555835a1b06d7f4aeb
Will Deacon <will.deacon@arm.com>
2015-12-10 16:05:36 +0000

    arm64: mm: ensure that the zero page is visible to the page table walker
    commit 32d6397805d00573ce1fa55f408ce2bca15b0ad3 upstream.
    
    In paging_init, we allocate the zero page, memset it to zero and then
    point TTBR0 to it in order to avoid speculative fetches through the
    identity mapping.
    
    In order to guarantee that the freshly zeroed page is indeed visible to
    the page table walker, we need to execute a dsb instruction prior to
    writing the TTBR.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	arch/arm64/mm/mmu.c

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -455,8 +455,11 @@ void __init paging_init(void)
 
 	bootmem_init();
 
-
 	dma_contiguous_remap();
+
+	/* Ensure the zero page is visible to the page table walker */
+	dsb();
+
 	/*
 	 * TTBR0 is only used for the identity mapping at this stage. Make it
 	 * point to zero page to avoid speculatively fetching new entries.
